ef79acb68d4a09c87fd8eb9113c8df6b37170e0f
TaoJun724 <1269088083@qq.com>
2020-04-30 22:26:06 +0800

    1

---
+++ 毕设源码/毕设源码/毕设server/毕设server/Comm.hpp
@@ -0,0 +1,41 @@
+﻿#ifndef  __COMM_H__
+#define  __COMM_H_
+
+
+#include<iostream>
+#include<Windows.h>
+#include<stdlib.h>
+#include<string.h>
+#include<map>
+#include<stdio.h>
+#include<winsock2.h>
+#include<WS2tcpip.h>
+#include <string.h>
+using namespace std;
+
+
+//数组的行列
+#define ROW 120
+#define COL 185
+#define AgvCarNum 9
+//颜色宏
+#define NONE "\033[m"
+#define RED "\033[0;32;31m"
+#define LIGHT_RED "\033[1;31m"
+#define GREEN  "\033[0;32;32m"
+#define BLUE "\033[0;32;34m"
+#define LIGHT_BLUE "\033[1;34m"
+#define DARY_GRAY "\033[1;30m"
+#define CYAN  "\033[0;36m"
+#define LIGHT_CYAN  "\033[1;36m"
+#define PURPLE "\033[0;35m"
+#define LIGHT_PURPLE "\033[1;35m"
+#define BROWN "\033[0;33m"
+#define YELLOW "\033[1;33m"
+#define LIGHT_GRAY "\033[0;37m"
+#define WHITE "\033[1;37m"
+
+
+
+
+#endif
